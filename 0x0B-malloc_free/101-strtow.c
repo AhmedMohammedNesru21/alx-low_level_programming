edc9428ff8bd78bbd1b39c58d5d4adacf6630215
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-07-24 09:58:09 +0000

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -45,18 +45,14 @@ void free_everything(char **string, int i)
  */
 char **strtow(char *str)
 {
-	int total_words = 0, b = 0, c = 0, length = 0;
+	int b = 0, c = 0, length = 0;
 	char **words, *found_word;
-
-	if (str == 0 || *str == 0)
+	if (str == 0 || *str == 0 || number(str)== 0)
 		return (NULL);
-	total_words = number(str);
-	if (total_words == 0)
-		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
+	words = malloc((number(str) + 1) * sizeof(char *));
 	if (words == 0)
 		return (NULL);
-	for (; *str != '\0' &&  b < total_words;)
+	for (; *str != '\0' &&  b < number(str);)
 	{
 		if (*str == ' ')
 			str++;
