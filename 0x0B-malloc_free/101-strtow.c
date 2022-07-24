071d4454f71d7ca3a22b1a6407e5d66e9158bfa8
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-07-24 11:49:19 +0000

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -72,7 +72,7 @@ char **strtow(char *str)
 				free_everything(words, b);
 				return (NULL);
 			}
-			for (; *found_word != ' ' && *found_word != '\0'; found_word++,c++)
+			for (; *found_word != ' ' && *found_word != '\0'; found_word++, c++)
 			{
 				words[b][c] = *found_word;
 			}
