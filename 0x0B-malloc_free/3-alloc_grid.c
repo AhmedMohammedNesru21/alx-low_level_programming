722c2fefd1d23cbed446f1d024e05b43b538307a
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-07-25 03:25:27 +0000

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,44 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two strings using at
- *                  most an inputted number of bytes.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of bytes of s2 to concatenate to s1.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the concatenated space in memory.
+ * array_range - create anarray of integers (min to max)
+ * @min: min number to start
+ * @max: max number to start
+ * Return: pointer
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+int *array_range(int min, int max)
 {
-	char *concat;
-	unsigned int len = n, index;
+  int *ptr;
+  int diff, i;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index]; index++)
-		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
-		return (NULL);
-
-	len = 0;
-
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
+  if (min > max)
+    return (NULL);
+  diff = max - min;
+  ptr = malloc((diff + 1) * sizeof(int));
+  if (ptr == NULL)
+    return (NULL);
+  for (i = 0; i <= diff; min++, i++)
+    {
+      ptr[i] = min;
+    }
+  return (ptr);
 }
