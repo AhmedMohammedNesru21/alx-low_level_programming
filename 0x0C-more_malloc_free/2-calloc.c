dd5cdbef9caf3084b920b31891d99be564cd1e23
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-07-28 21:56:45 +0000

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,43 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- *_calloc - allocate memory for an array usingmalloc
- * @nmemb: number ofelements
- * @size: size of bytes
- * Return: pointer
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: input size 1
+ * @size: input size bytes
+ * Return: nothing
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  void *ptr;
+	void *cal;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  ptr = malloc(nmemb * size);
-  if (ptr == NULL)
-    return (NULL);
-  _memset(ptr, 0, (nmemb * size));
-  return (ptr);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	cal = malloc(nmemb * size);
+	if (cal == NULL)
+		return (NULL);
+	_memset(cal, 0, nmemb * size);
+	return (cal);
 }
-
 /**
- *_memset - sets first n bytes of the memory area
- * @s: array to set
- * @b: what to set it to
- * @n: first n bytes
- * Return: s
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: mamory area to return
+ * @b: constant byte
+ * @n: size of bytes
+ * Return: char
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i;
+	unsigned int i = 0;
 
-  for (i = 0; i < n; i++)
-    {
-      s[i] = b;
-    }
-  return (s);
+	while (i < n)
+	{
+
+		*(s + i) = b;
+		i += 1;
+	}
+	return (s);
 }
