7bb38f032598c399d086b981690c6e29d07075fd
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-09-30 01:59:07 +0000

    Task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,22 +1,18 @@
-// /*
- * File: 4-free_dlistint.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
-
 /**
- * free_dlistint - Frees a linked dlistint_t list.
- * @head: The head of the dlistint_t list.
- */
+  * free_dlistint - frees a linked list
+  * @head: head of the list
+  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 
 	while (head)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		tmp = head;
+		head = head->next;
+		tmp->prev = NULL;
+		tmp->next = NULL;
+		free(tmp);
 	}
 }
